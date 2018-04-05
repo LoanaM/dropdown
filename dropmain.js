5696cff8ab6cf80302d0a6765bc483f765abe4a1
LoanaWitch <loanawitch@live.com>
2018-04-05 20:34:35 +0200

    dropdown + freccia fa

--- dropmain.js
+++ dropmain.js
@@ -3,10 +3,12 @@ var clicked = false;
 $('#home').click(function(){
   if(!clicked) {
     $(this).children('.tendina').show();
+    $(this).children('.faicon').show();
     clicked = true;
   }
   else {
     $(this).children('.tendina').hide();
+    $(this).children('.faicon').hide()
     clicked = false;
   }
 });
@@ -14,22 +16,28 @@ $('#home').click(function(){
 //mouse su menu
 $('#libri').mouseenter(function(){
   $(this).children('.tendina').show(2000);
+  $(this).children('.faicon').show(2000);
 });
 $('#libri').mouseleave(function(){
   $(this).children('.tendina').hide();
+  $(this).children('.faicon').hide();
+
 });
 
 //mouse sul link e fade
 $('#audio').mouseenter(function(){
   $(this).parents().children('.tendina').fadeIn();
+  $(this).parents().children('.faicon').fadeIn();
 });
 $('#audio').mouseleave(function(){
   $(this).parents().children('.tendina').fadeOut();
+  $(this).parents().children('.faicon').fadeOut();
 });
 
 //click sul menu e toggle
 $('#dvd').click(function(){
   $(this).children('.tendina').toggle();
+  $(this).children('.faicon').toggle();
 });
 
 
@@ -37,7 +45,9 @@ $('#dvd').click(function(){
 $('#offer').on({
   mouseenter: function(){
     $(this).parents().children('.tendina').fadeIn(3000);
+    $(this).parents().children('.faicon').show();
   }, mouseleave: function(){
     $(this).parents().children('.tendina').fadeOut("slow");
+    $(this).parents().children('.faicon').hide();
   }
 });
