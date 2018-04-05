e16387a4f291f1d425c0185b980f94cc8bdaee20
LoanaWitch <loanawitch@live.com>
2018-04-05 20:15:18 +0200

    different combination

--- dropmain.js
+++ dropmain.js
@@ -1,3 +1,4 @@
+//click su menu
 var clicked = false;
 $('#home').click(function(){
   if(!clicked) {
@@ -10,9 +11,33 @@ $('#home').click(function(){
   }
 });
 
+//mouse su menu
 $('#libri').mouseenter(function(){
   $(this).children('.tendina').show(2000);
 });
 $('#libri').mouseleave(function(){
   $(this).children('.tendina').hide();
+});
+
+//mouse sul link e fade
+$('#audio').mouseenter(function(){
+  $(this).parents().children('.tendina').fadeIn();
+});
+$('#audio').mouseleave(function(){
+  $(this).parents().children('.tendina').fadeOut();
+});
+
+//click sul menu e toggle
+$('#dvd').click(function(){
+  $(this).children('.tendina').toggle();
+});
+
+
+//mouse sul link e fade
+$('#offer').on({
+  mouseenter: function(){
+    $(this).parents().children('.tendina').fadeIn(3000);
+  }, mouseleave: function(){
+    $(this).parents().children('.tendina').fadeOut("slow");
+  }
 });
